0aebf40966f5db65231dc996686ad9ae22887ef6
AhmedEmad-21 <ahmedand3509@gmail.com>
2025-07-18 21:13:45 +0300

    Update

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className="text-4xl md:text-5xl font-bold mb-2 underline underline-offset-4 decoration-blue-500"
+        className="text-4xl md:text-5xl font-bold mb-2 mt-2 underline underline-offset-4 decoration-blue-500"
       >
         My Skills
       </motion.h2>
